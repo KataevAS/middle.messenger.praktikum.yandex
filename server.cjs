8d20e056368bcbccac653e63abd6f70980e7d930
kataev <kataev@rg.ru>
2023-08-30 16:48:31 +0300

    test

--- server.cjs
+++ server.cjs
@@ -5,6 +5,10 @@ const PORT = 3000
 
 app.use(express.static(`${__dirname}/dist/`))
 
+app.get('/', (req, res) => {
+  res.status(200)
+})
+
 app.get('*', (req, res) => {
   res.status(200).sendFile(`${__dirname}/dist/`)
 })
