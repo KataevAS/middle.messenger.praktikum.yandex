febc819cd6df411f66d75532cac72e270f70688e
kataev <kataev@rg.ru>
2023-08-30 16:18:03 +0300

    fix status response

--- server.cjs
+++ server.cjs
@@ -6,7 +6,7 @@ const PORT = 3000
 app.use(express.static(`${__dirname}/dist/`))
 
 app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/dist/`)
+  res.status(200).sendFile(`${__dirname}/dist/`)
 })
 
 app.listen(PORT, function () {
